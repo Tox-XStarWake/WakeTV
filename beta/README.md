75fb1f3bf64e3a82a6a60c3e4037421ee7ecf870
Glitch (www-xstarwake-org) <none>
2023-02-28 04:38:50 +0000

    Import of Tox-XStarWake/WakeTV ./index.html:41330950/193

--- beta/README.md
+++
@@ -1,6 +0,0 @@
-# Heat
-Front end effects viewer for Heat.
-
-## Help
-
-Visit the [Wiki](https://github.com/scottgarner/Heat/wiki) or [Discord](https://discord.gg/GtAffRP).
--- beta/js/demo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-{
-    const params = (new URL(document.location)).searchParams;
-    const demo = params.get("demo") || false;
-    const clear = params.get("clear") || false;
-
-    if ((!window.obsstudio && !clear) || demo) document.body.classList.add("demo");
-}
