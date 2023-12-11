c084a9d2ad9ae24c02caa559c2e33481ec4fdc7c
Glitch (www-xstarwake-org) <none>
2023-12-11 15:59:55 +0000

    Import of Tox-XStarWake/WakeTV

--- PIR/script.js
+++ PIR/script.js
@@ -1,24 +1,26 @@
 let spinning = false;
+let winningNumber = 42; // Set your desired winning number
 
 // Function to fetch and parse data from the text file
 async function fetchData() {
-  try {
-    const response = await fetch('data.txt'); // Update with your actual file path
-    const data = await response.text();
-    return data.split('\n').map(line => {
-      const [name, number] = line.split(': ');
-      return { name, number: parseInt(number) };
-    });
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return [];
+    try {
+      const response = await fetch('data.txt'); // Update with your actual file path
+      const data = await response.text();
+      return data.split('\n').map(line => {
+        const [name, number] = line.split(': ');
+        return { name, number: parseInt(number) };
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return [];
+    }
   }
-}
-
-// Function to sort the data by number
-function sortDataByNumber(data) {
-  return data.sort((a, b) => a.number - b.number);
-}
+  
+  // Function to sort the data by number
+  function sortDataByNumber(data) {
+    return data.sort((a, b) => a.number - b.number);
+  }
+  
 
 function spinWheel() {
   if (!spinning) {
@@ -29,7 +31,10 @@ function spinWheel() {
       
       wheel.innerHTML = sections.join('');
 
-      const randomDegree = Math.floor(Math.random() * 360) + 1440; // Adjust this range as needed
+      // Find the section that corresponds to or is closest to the winning number
+      const closestSection = getClosestSection(winningNumber, sortedData);
+      const randomDegree = (closestSection.index * (360 / sortedData.length)) + 720; // Adjust this range as needed
+
       spinning = true;
 
       wheel.style.transform = `rotate(${randomDegree}deg)`;
@@ -41,6 +46,16 @@ function spinWheel() {
       }, 3000); // Adjust this time to match the wheel's transition time
     });
   }
+}
+
+function getClosestSection(targetNumber, data) {
+  // Find the section with the number closest to the targetNumber
+  const closestEntry = data.reduce((closest, entry) => {
+    const diff = Math.abs(entry.number - targetNumber);
+    return (diff < closest.diff) ? { entry, diff } : closest;
+  }, { entry: null, diff: Infinity });
+
+  return { entry: closestEntry.entry, index: data.indexOf(closestEntry.entry) };
 }
 
 // The getSelectedSection function remains the same
